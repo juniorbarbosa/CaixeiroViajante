ac2885da8c2eb69ab3e78b131c0bf3ab36316614
Junior <Junior@Junior-PC>
2015-11-18 21:55:08 -0200

    Commit inicial.

---
+++ CaixeiroViajante/src/br/univel/Principal.java
@@ -0,0 +1,20 @@
+package br.univel;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		menu();
+		escolhaMenu();
+	}
+
+	private static void menu() {
+		System.out.println("Menu principal");
+		System.out.println("1 - Lista de Cidades");
+		System.out.println("2 - Ligação e distância");
+		System.out.println("3 - Percorrer");
+	}
+
+	private static void escolhaMenu() {
+
+	}
+}
