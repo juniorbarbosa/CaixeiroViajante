fa56f4e7f2962bbf09b7388a327e44643f3540ea
Junior <Junior@Junior-PC>
2015-11-18 22:04:23 -0200

    Alterado menu principal.

--- CaixeiroViajante/src/br/univel/Principal.java
+++ CaixeiroViajante/src/br/univel/Principal.java
@@ -1,20 +1,44 @@
 package br.univel;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		menu();
-		escolhaMenu();
 	}
 
 	private static void menu() {
+		int opcao;
+
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Menu principal");
 		System.out.println("1 - Lista de Cidades");
 		System.out.println("2 - Ligação e distância");
 		System.out.println("3 - Percorrer");
+		System.out.println("9 - Sair");
+		System.out.println("Digite sua opção: ");
+
+		opcao = sc.nextInt();
+
+		switch (opcao) {
+		case 1:
+			System.out.println("Lista de cidades");
+			menu();
+		case 2:
+			System.out.println("Ligação e distância");
+			menu();
+		case 3:
+			System.out.println("Percorrer");
+			menu();
+		case 9:
+			System.exit(0);
+		default:
+			System.out.println("\nOPÇÃO INVALIDA, TENTE NOVAMENTE...\n");
+			menu();
+			break;
+		}
 	}
 
-	private static void escolhaMenu() {
-
-	}
 }
