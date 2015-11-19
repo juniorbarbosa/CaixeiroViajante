ba6f3c4ef6a5b93d19106f4eef37968211890610
Junior <Junior@Junior-PC>
2015-11-18 22:05:37 -0200

    teste

--- CaixeiroViajante/src/br/univel/Principal.java
+++ CaixeiroViajante/src/br/univel/Principal.java
@@ -18,6 +18,7 @@ public class Principal {
 		System.out.println("2 - Ligação e distância");
 		System.out.println("3 - Percorrer");
 		System.out.println("9 - Sair");
+		System.out.println("9 - Sair");
 		System.out.println("Digite sua opção: ");
 
 		opcao = sc.nextInt();
